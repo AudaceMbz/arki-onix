eaa9c7ee6a75ace8456d0d5c4840865c719ffefe
Audacembz <audacembz@gmail.com>
2026-04-17 23:40:39 +0200

    Fix Unauthorized session error

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ app.use(session({
   resave: false,
   saveUninitialized: false,
   cookie: { 
-    secure: process.env.NODE_ENV === 'production', 
+    secure: false, // Set to false for better browser compatibility during testing
     maxAge: 24 * 60 * 60 * 1000 
   }
 }));
